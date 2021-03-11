95615522867dd7889c7b29ff27ebb2b2d940ab64
seung <seunghee9941@gmail.com>
2021-03-11 19:29:09 +0900

    1764번: 듣보잡 update

--- BOJ/B_1764.py
+++ BOJ/B_1764.py
@@ -1,11 +1,13 @@
-N, M = map(int, input().split())
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
 N_name = set()
 M_name = set()
 
-for i in range(N):
-    N_name.add(input())
-for i in range(M):
-    M_name.add(input())
+for _ in range(N):
+    N_name.add(sys.stdin.readline().rstrip())
+for _ in range(M):
+    M_name.add(sys.stdin.readline().rstrip())
 
 name = sorted(list(N_name & M_name))
 
